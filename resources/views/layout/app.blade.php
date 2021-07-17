42e9214240729aa36e9ffde2602b9fb92b051c1f
installerJOE <justiceigoma@gmail.com>
2021-07-17 14:26:25 +0100

    Laravel Setup Files

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+    
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    {{-- <link  href="/path/to/cropper.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{asset('croppie/croppie.css')}}" />
+
+    <!-- Styles -->
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{asset("css/style.css")}}">
+    <link rel="stylesheet" href="{{asset("css/sidebar.css")}}"> --}}
+    <link rel="stylesheet" href="{{ url('public/css/app.css') }}">
+    <link rel="stylesheet" href="{{url('public/css/style.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/sidebar.css')}}">
+
+    {{-- Additional meta data such as title and other stylesheets --}}
+    @yield('meta-content')
+    
+    <style>
+      .card{
+        margin-bottom:2em;
+      }
+      .form-auth > div{
+        margin-bottom: 1.5em !important;
+      }
+      nav{
+        margin:0px !important;
+      }
+      /* Ensure the size of the image fit the container perfectly */
+      img {
+        display: block;
+
+        /* This rule is very important, please don't ignore this */
+        max-width: 100%;
+      }
+      #app{
+        padding-top:3em;
+      }
+    </style>
+  </head>
+  <body>
+
+    <div>
+      @yield('content')
+    </div>
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{asset('croppie/croppie.js')}}"></script>
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+    <script src="{{asset('js/script.js')}}"></script>
+    
+  </body>
+</html>
+
+
---
+++ resources/views/public/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>This is the Home Page</h1>
+@endsection
